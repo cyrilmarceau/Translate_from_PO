a08c5ae035c128b7c677c8eca0481b26a1a1e6ad
cyrilmarceau <marceau.cyril@hotmail.fr>
2021-02-17 23:56:39 +0100

    get language and create file name  with correct code lang

--- make-json.sh
+++ make-json.sh
@@ -1,12 +1,12 @@
 #! /bin/bash
-echo "{" > translate.json
+language=$(cat translate.po | grep 'Language:'  | sed "s/Language: //" | sed -e 's/^"//' -e 's/"$//' | sed 's/\\/ /g' | tr -d ‘\n’ | xargs) 
+echo $language
+echo "{" > "translate-${language}.json"
 	while read line;
 		do
-			echo -n "$line" | grep "msgid" | sed "s/msgid //" | sed 's/$/:/' | tr -d '\n' >> translate.json
-			echo -n "$line" | grep "msgstr" | sed "s/msgstr //" | sed 's/$/,/' >> translate.json
+			echo -n "$line" | grep "msgid" | sed "s/msgid //" | sed 's/$/:/' | tr -d '\n' >> "translate-${language}.json"
+			echo -n "$line" | grep "msgstr" | sed "s/msgstr //" | sed 's/$/,/' >> "translate-${language}.json"
 		done < translate.po
 	
-	sed -zr -i 's/,([^,]*$)/\1/' translate.json
-echo "}" >> translate.json
-
-cat translate.json
+	sed -zr -i 's/,([^,]*$)/\1/' "translate-${language}.json"
+echo "}" >> "translate-${language}.json"
